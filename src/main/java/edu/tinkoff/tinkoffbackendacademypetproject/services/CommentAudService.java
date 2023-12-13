ae830fa894e1ae4ebb5fecb5cc4cb271ed0fe9c2
Artem Lysenko <artemlysenko@niuitmo.ru>
2023-12-13 16:55:07 +0300

    work on local

---
+++ src/main/java/edu/tinkoff/tinkoffbackendacademypetproject/services/CommentAudService.java
@@ -0,0 +1,20 @@
+package edu.tinkoff.tinkoffbackendacademypetproject.services;
+
+
+import edu.tinkoff.tinkoffbackendacademypetproject.model.CommentAudit;
+import edu.tinkoff.tinkoffbackendacademypetproject.model.CommentEntity;
+import edu.tinkoff.tinkoffbackendacademypetproject.repositories.CommentAudRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentAudService {
+    private final CommentAudRepository commentAudRepository;
+
+    public List<CommentAudit> getRevisionForComment(Long commentId) {
+        return commentAudRepository.getRevisionForComment(commentId);
+    }
+}
